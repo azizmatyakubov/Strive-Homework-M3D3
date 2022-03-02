ea14d789dd1afe2f5195a3448a22a304b44dd493
AZIZ <a.matyaqubov1471@gmail.com>
2022-03-02 15:15:15 +0100

    update version

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 const API_KEY = '563492ad6f9170000100000101721bd9fedd4817b2ed585ad4ebc93a'
+const URL = 'https://api.pexels.com/v1/search?query=people'
+
+
+// working 
+fetch(URL, { headers: { Authorization: API_KEY }})
+    .then(response=>response.json())
+    .then(result=>{
+        console.log(result)
+    })
+    .catch(err=>{
+        console.log(err)
+    })
+
